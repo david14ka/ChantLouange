2ca84b1b0284ed89980ab6f5009b051c40288523
davidkazad <14ka135@gmail.com>
2020-05-23 23:56:49 +0200

    cc verification

--- app/src/main/java/com/davidkazad/chantlouange/songs/Test.java
+++ app/src/main/java/com/davidkazad/chantlouange/songs/Test.java
@@ -23,30 +23,8 @@ public class Test {
         //System.out.println(pageList.size());
 
         /***
-         * 22A
-         * 71A
-         * 73A
-         * 80A
-         * 83A
-         * 84A
-         * 90A
-         * 97A
-         * 130A
-         * 132A
-         * 136A
-         * 156A
-         * 157 NOT EXIST
-         * 160A
-         * 162B
-         * 191A
-         * 229A
-         * 256A
-         * 258B
-         * 296A
-         * 329A
-         * 337B
-         * 339A
-         * 342A
+         * 74 NOT EXIST
+         * 86 NOT EXIST
          *
           ***/
 
